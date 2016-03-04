cc48c333eb632cf40a6fdb647a8b61b41a136f54
Barbara Holt <worldwidebarbara@yahoo.com>
2016-03-04 00:01:15 -0500

    Layout Update 4 - configure layout sizing for responsiveness across differently sized devices
    Content Update 4
    - add social media buttons and links for newly created social pages
    - update header to include jagged Baltimore skyline

---
+++ js/buttons.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('.eventbrite').click(function () {
+        window.open('https://www.eventbrite.com/e/abilities-hackathon-tickets-22285583781', '_blank');
+    });
+});
--- js/scroll-magic.js
+++ js/scroll-magic.js
@@ -25,4 +25,31 @@ $(document).ready(function () {
         .setTween("#who > div", {y: "80%", ease: Linear.easeNone})
         //.addIndicators()
         .addTo(controller);
+    /*function pathPrepare ($el) {
+        var lineLength = $el[0].getTotalLength();
+        $el.css("stroke-dasharray", lineLength);
+        $el.css("stroke-dashoffset", lineLength);
+    }
+
+    var $word = $("path#word");
+    var $dot = $("path#dot");
+
+    // prepare SVG
+    pathPrepare($word);
+    pathPrepare($dot);
+
+    // init controller
+    var controller = new ScrollMagic.Controller();
+
+    // build tween
+    var tween = new TimelineMax()
+        .add(TweenMax.to($word, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
+        .add(TweenMax.to($dot, 0.1, {strokeDashoffset: 0, ease:Linear.easeNone}))  // draw dot for 0.1
+        .add(TweenMax.to("path", 1, {stroke: "#33629c", ease:Linear.easeNone}), 0);			// change color during the whole thing
+
+    // build scene
+    var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 200, tweenChanges: true})
+        .setTween(tween)
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);*/
 });
