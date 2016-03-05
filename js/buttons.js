4fa925055ea0b29a354c7fe97d0548e943f254c5
Barbara Holt <worldwidebarbara@yahoo.com>
2016-03-05 17:13:27 -0500

    Content Update 4 - fix eventbrite button links

--- js/buttons.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $('.eventbrite').click(function () {
-        window.open('https://www.eventbrite.com/e/abilities-hackathon-tickets-22285583781', '_blank');
-    });
-});
